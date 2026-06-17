dad467f6dab4fc9b2c6897fe0d4a2af41f5a85e7
SHUBHAM SETH <shubham.seth@greenpaynetwork.co.in>
2026-06-17 16:10:06 +0530

    The Problem with OnPush, Cross-Component Data & Not Using Signals

--- src/app/messages/messages.service.ts
+++ src/app/messages/messages.service.ts
@@ -4,10 +4,12 @@ import { Injectable, signal } from '@angular/core';
   providedIn: 'root',
 })
 export class MessagesService {
-  private messages = signal<string[]>([]);
-  allMessages = this.messages.asReadonly();
+  private messages: string[] = [];
+  get allMessages() {
+    return [...this.messages]
+  }
 
   addMessage(message: string) {
-    this.messages.update((prevMessages) => [...prevMessages, message]);
+    this.messages = [...this.messages, message];
   }
 }
